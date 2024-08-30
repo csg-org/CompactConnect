e05280e71b9f8b235b292533f5fbfca5914fb71f
Justin Frahm <justin@inspiringapps.com>
2024-08-30 09:43:15 -0600

    WIP

--- backend/compact-connect/stacks/api_stack/v1_api/api.py
+++ backend/compact-connect/stacks/api_stack/v1_api/api.py
@@ -8,6 +8,7 @@ from stacks.api_stack.v1_api.bulk_upload_url import BulkUploadUrl
 from stacks.api_stack.v1_api.query_providers import QueryProviders
 
 from .post_licenses import PostLicenses
+from .staff_users import StaffUsers
 
 
 class V1Api:
@@ -25,6 +26,10 @@ class V1Api:
         ]
         write_scopes = [
             f'{resource_server}/write'
+            for resource_server in persistent_stack.staff_users.resource_servers.keys()
+        ]
+        admin_scopes = [
+            f'{resource_server}/admin'
             for resource_server in persistent_stack.staff_users.resource_servers.keys()
         ]
         read_auth_method_options = MethodOptions(
@@ -67,3 +72,17 @@ class V1Api:
             method_options=write_auth_method_options,
             bulk_uploads_bucket=persistent_stack.bulk_uploads_bucket
         )
+
+        # /v1/staff-users
+        staff_users_resource = compact_resource.add_resource('staff-users')
+        # GET    /v1/staff-users
+        # POST   /v1/staff-users
+        # GET    /v1/staff-users/me
+        # PATCH  /v1/staff-users/me
+        # GET    /v1/staff-users/{userId}
+        # PATCH  /v1/staff-users/{userId}
+        StaffUsers(
+            resource=staff_users_resource,
+            admin_scopes=admin_scopes
+        )
+
---
+++ backend/compact-connect/stacks/api_stack/v1_api/staff_users.py
@@ -0,0 +1,417 @@
+from __future__ import annotations
+
+import json
+import os
+from functools import cached_property
+from typing import List
+
+from aws_cdk.aws_apigateway import Resource, MethodResponse, JsonSchema, \
+    JsonSchemaType, MockIntegration, IntegrationResponse, AuthorizationType
+
+# Importing module level to allow lazy loading for typing
+from .. import cc_api
+
+
+class StaffUsers:
+    def __init__(
+            self, *,
+            resource: Resource,
+            admin_scopes: List[str]
+    ):
+        super().__init__()
+
+        self.resource = resource
+        self.api: cc_api.CCApi = resource.api
+        self.log_groups = []
+
+        # GET .../
+        self._add_get_users(self.resource, admin_scopes)
+
+        user_id_resource = self.resource.add_resource('{userId}')
+        # GET .../{userId}
+        self._add_get_user(user_id_resource, admin_scopes)
+        self._add_patch_user(user_id_resource, admin_scopes)
+
+        me_resource = self.resource.add_resource('me')
+        # GET .../me
+        self._add_get_me(me_resource)
+        # PATCH .../me
+        self._add_patch_me(me_resource)
+
+        self.api.log_groups.extend(self.log_groups)
+
+    def _add_get_me(self, me_resource: Resource):
+        with open(os.path.join('lambdas', 'staff-users', 'tests', 'resources', 'me.json')) as f:
+            response_template = f.read()
+
+        me_resource.add_method(
+            'GET',
+            request_validator=self.api.parameter_body_validator,
+            method_responses=[
+                MethodResponse(
+                    status_code='200',
+                    response_models={
+                        'application/json': self.get_me_model
+                    }
+                )
+            ],
+            integration=MockIntegration(
+                integration_responses=[
+                    IntegrationResponse(
+                        status_code='200',
+                        response_templates={
+                            'application/json': response_template
+                        }
+                    )
+                ]
+            ),
+            request_parameters={
+                'method.request.header.Authorization': True
+            },
+            authorization_type=AuthorizationType.COGNITO,
+            authorizer=self.api.staff_users_authorizer,
+            authorization_scopes=["profile"]
+        )
+
+    def _add_patch_me(self, me_resource: Resource):
+        with open(os.path.join('lambdas', 'staff-users', 'tests', 'resources', 'me.json')) as f:
+            response_template = f.read()
+
+        me_resource.add_method(
+            'PATCH',
+            request_validator=self.api.parameter_body_validator,
+            request_models={
+                'application/json': self.patch_me_model
+            },
+            method_responses=[
+                MethodResponse(
+                    status_code='200',
+                    response_models={
+                        'application/json': self.get_me_model
+                    }
+                )
+            ],
+            integration=MockIntegration(
+                integration_responses=[
+                    IntegrationResponse(
+                        status_code='200',
+                        response_templates={
+                            'application/json': response_template
+                        }
+                    )
+                ]
+            ),
+            request_parameters={
+                'method.request.header.Authorization': True
+            },
+            authorization_type=AuthorizationType.COGNITO,
+            authorizer=self.api.staff_users_authorizer,
+            # This is the same scope that Cognito requires for editing its own user attributes, so we will follow suit.
+            authorization_scopes=["aws.cognito.signin.user.admin"]
+        )
+
+    def _add_get_users(self, users_resource: Resource, admin_scopes: List[str]):
+        with open(os.path.join('lambdas', 'staff-users', 'tests', 'resources', 'me.json')) as f:
+            user_data = json.load(f)
+        response = {
+            'users': [user_data],
+            'pagination': {
+                'pageSize': 10
+            }
+        }
+
+        users_resource.add_method(
+            'GET',
+            request_validator=self.api.parameter_body_validator,
+            method_responses=[
+                MethodResponse(
+                    status_code='200',
+                    response_models={
+                        'application/json': self.get_me_model
+                    }
+                )
+            ],
+            integration=MockIntegration(
+                integration_responses=[
+                    IntegrationResponse(
+                        status_code='200',
+                        response_templates={
+                            'application/json': json.dumps(response)
+                        }
+                    )
+                ]
+            ),
+            request_parameters={
+                'method.request.header.Authorization': True
+            },
+            authorization_type=AuthorizationType.COGNITO,
+            authorizer=self.api.staff_users_authorizer,
+            authorization_scopes=admin_scopes
+        )
+
+    def _add_get_user(self, user_id_resource: Resource, admin_scopes: List[str]):
+        with open(os.path.join('lambdas', 'staff-users', 'tests', 'resources', 'me.json')) as f:
+            response_template = f.read()
+
+        user_id_resource.add_method(
+            'GET',
+            request_validator=self.api.parameter_body_validator,
+            method_responses=[
+                MethodResponse(
+                    status_code='200',
+                    response_models={
+                        'application/json': self.get_me_model
+                    }
+                )
+            ],
+            integration=MockIntegration(
+                integration_responses=[
+                    IntegrationResponse(
+                        status_code='200',
+                        response_templates={
+                            'application/json': response_template
+                        }
+                    )
+                ]
+            ),
+            request_parameters={
+                'method.request.header.Authorization': True
+            },
+            authorization_type=AuthorizationType.COGNITO,
+            authorizer=self.api.staff_users_authorizer,
+            authorization_scopes=admin_scopes
+        )
+
+    def _add_patch_user(self, me_resource: Resource, admin_scopes: List[str]):
+        with open(os.path.join('lambdas', 'staff-users', 'tests', 'resources', 'me.json')) as f:
+            response_template = f.read()
+
+        me_resource.add_method(
+            'PATCH',
+            request_validator=self.api.parameter_body_validator,
+            request_models={
+                'application/json': self.patch_user_model
+            },
+            method_responses=[
+                MethodResponse(
+                    status_code='200',
+                    response_models={
+                        'application/json': self.get_me_model
+                    }
+                )
+            ],
+            integration=MockIntegration(
+                integration_responses=[
+                    IntegrationResponse(
+                        status_code='200',
+                        response_templates={
+                            'application/json': response_template
+                        }
+                    )
+                ]
+            ),
+            request_parameters={
+                'method.request.header.Authorization': True
+            },
+            authorization_type=AuthorizationType.COGNITO,
+            authorizer=self.api.staff_users_authorizer,
+            authorization_scopes=admin_scopes
+        )
+
+    @cached_property
+    def get_me_model(self):
+        """
+        Return the Get Me Model, which should only be created once per API
+        """
+        if hasattr(self.api, 'v1_get_me_model'):
+            return self.api.v1_get_me_model
+
+        self.api.v1_get_me_model = self.api.add_model(
+            'V1GetMeModel',
+            description='Get me response model',
+            schema=self._user_schema
+        )
+        return self.api.v1_get_me_model
+
+    @cached_property
+    def get_staff_users_response_model(self):
+        """
+        Return the Get Users Model, which should only be created once per API
+        """
+        if hasattr(self.api, 'v1_get_staff_users_response_model'):
+            return self.api.v1_get_staff_users_response_model
+
+        self.api.v1_get_staff_users_response_model = self.api.add_model(
+            'V1GetStaffUsersModel',
+            description='Get staff users response model',
+            schema=JsonSchema(
+                type=JsonSchemaType.OBJECT,
+                additional_properties=False,
+                properties={
+                    'users': JsonSchema(
+                        type=JsonSchemaType.ARRAY,
+                        items=self._user_schema
+                    ),
+                    'pagination': self._pagination_response_schema
+                }
+            )
+        )
+        return self.api.v1_get_staff_users_response_model
+
+    @cached_property
+    def patch_me_model(self):
+        """
+        Return the Get Me Model, which should only be created once per API
+        """
+        if hasattr(self.api, 'v1_patch_me_request_model'):
+            return self.api.v1_patch_me_request_model
+
+        self.api.v1_patch_me_request_model = self.api.add_model(
+            'V1PatchMeRequestModel',
+            description='Patch me request model',
+            schema=JsonSchema(
+                type=JsonSchemaType.OBJECT,
+                additional_properties=False,
+                properties={
+                    'attributes': JsonSchema(
+                        type=JsonSchemaType.OBJECT,
+                        additional_properties=False,
+                        properties={
+                            'email': JsonSchema(type=JsonSchemaType.STRING, min_length=5, max_length=100),
+                            'givenName': JsonSchema(type=JsonSchemaType.STRING, min_length=1, max_length=100),
+                            'familyName': JsonSchema(type=JsonSchemaType.STRING, min_length=1, max_length=100)
+                        }
+                    )
+                }
+            )
+        )
+        return self.api.v1_patch_me_request_model
+
+    @cached_property
+    def patch_user_model(self):
+        """
+        Return the Patch Me Model, which should only be created once per API
+        """
+        if hasattr(self.api, 'v1_patch_user_request_model'):
+            return self.api.v1_patch_user_request_model
+
+        self.api.v1_patch_user_request_model = self.api.add_model(
+            'V1PatchUserRequestModel',
+            description='Patch user request model',
+            schema=JsonSchema(
+                type=JsonSchemaType.OBJECT,
+                additional_properties=False,
+                properties={
+                    'permissions': JsonSchema(
+                        type=JsonSchemaType.OBJECT,
+                        additional_properties=JsonSchema(
+                            type=JsonSchemaType.OBJECT,
+                            additional_properties=False,
+                            properties={
+                                'actions': JsonSchema(
+                                    type=JsonSchemaType.OBJECT,
+                                    properties={
+                                        'read': JsonSchema(type=JsonSchemaType.BOOLEAN),
+                                        'admin': JsonSchema(type=JsonSchemaType.BOOLEAN)
+                                    }
+                                ),
+                                'jurisdictions': JsonSchema(
+                                    type=JsonSchemaType.OBJECT,
+                                    additional_properties=JsonSchema(
+                                        type=JsonSchemaType.OBJECT,
+                                        properties={
+                                            'actions': JsonSchema(
+                                                type=JsonSchemaType.OBJECT,
+                                                additional_properties=False,
+                                                properties={
+                                                    'write': JsonSchema(type=JsonSchemaType.BOOLEAN),
+                                                    'admin': JsonSchema(type=JsonSchemaType.BOOLEAN)
+                                                }
+                                            )
+                                        }
+                                    )
+                                )
+                            }
+                        )
+                    )
+                }
+            )
+        )
+        return self.api.v1_patch_user_request_model
+
+    @property
+    def _user_schema(self):
+        return JsonSchema(
+            type=JsonSchemaType.OBJECT,
+            required=[
+                'userId',
+                'attributes',
+                'permissions'
+            ],
+            additional_properties=False,
+            properties={
+                'userId': JsonSchema(type=JsonSchemaType.STRING),
+                'attributes': JsonSchema(
+                    type=JsonSchemaType.OBJECT,
+                    additional_properties=True,
+                    required=['email', 'givenName', 'familyName'],
+                    properties={
+                        'email': JsonSchema(type=JsonSchemaType.STRING),
+                        'givenName': JsonSchema(type=JsonSchemaType.STRING),
+                        'familyName': JsonSchema(type=JsonSchemaType.STRING)
+                    }
+                ),
+                'permissions': JsonSchema(
+                    type=JsonSchemaType.OBJECT,
+                    additional_properties=JsonSchema(
+                        type=JsonSchemaType.OBJECT,
+                        additional_properties=False,
+                        properties={
+                            'actions': JsonSchema(
+                                type=JsonSchemaType.OBJECT,
+                                properties={
+                                    'read': JsonSchema(type=JsonSchemaType.BOOLEAN),
+                                    'admin': JsonSchema(type=JsonSchemaType.BOOLEAN)
+                                }
+                            ),
+                            'jurisdictions': JsonSchema(
+                                type=JsonSchemaType.OBJECT,
+                                additional_properties=JsonSchema(
+                                    type=JsonSchemaType.OBJECT,
+                                    properties={
+                                        'actions': JsonSchema(
+                                            type=JsonSchemaType.OBJECT,
+                                            additional_properties=False,
+                                            properties={
+                                                'write': JsonSchema(type=JsonSchemaType.BOOLEAN),
+                                                'admin': JsonSchema(type=JsonSchemaType.BOOLEAN)
+                                            }
+                                        )
+                                    }
+                                )
+                            )
+                        }
+                    )
+                )
+            }
+        )
+
+    @property
+    def _pagination_response_schema(self):
+        return JsonSchema(
+            type=JsonSchemaType.OBJECT,
+            properties={
+                'lastKey': JsonSchema(
+                    type=[JsonSchemaType.STRING, JsonSchemaType.NULL],
+                    min_length=1,
+                    max_length=1024
+                ),
+                'prevLastKey': JsonSchema(
+                    type=[JsonSchemaType.STRING, JsonSchemaType.NULL],
+                    min_length=1,
+                    max_length=1024
+                ),
+                'pageSize': JsonSchema(type=JsonSchemaType.INTEGER, minimum=5, maximum=100)
+            }
+        )
+
