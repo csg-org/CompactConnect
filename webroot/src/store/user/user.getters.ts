4a9f0c92a69fec678bd120c6605b8f9f4f3af3c7
Dana Stiefel <dana@inspiringapps.com>
2024-09-24 11:38:58 -0600

    fix

--- webroot/src/store/user/user.getters.ts
+++ webroot/src/store/user/user.getters.ts
@@ -9,7 +9,7 @@ import { authStorage, AuthTypes, tokens } from '@/app.config';
 export default {
     state: (state: any) => state,
     currentCompact: (state: any) => state.currentCompact,
-    authType: () => async () => {
+    authType: () => () => {
         let loggedInAsType = '';
 
         if (authStorage.getItem(tokens[AuthTypes.STAFF]?.AUTH_TOKEN)) {
