cd3da998b837b5805e13e6e2134d213cb5f391a8
John Sandoval <john@inspiringapps.com>
2024-10-02 14:35:39 -0600

    Initial staff user profile & multi-compact handling

--- webroot/src/components/Page/PageHeader/PageHeader.ts
+++ webroot/src/components/Page/PageHeader/PageHeader.ts
@@ -5,18 +5,27 @@
 //  Created by InspiringApps on 5/4/2020.
 //
 
+import { AuthTypes } from '@/app.config';
 import { Component, Vue, toNative } from 'vue-facing-decorator';
+import CompactSelector from '@components/CompactSelector/CompactSelector.vue';
 import PageNav from '@components/Page/PageNav/PageNav.vue';
 import PageMainNav from '@components/Page/PageMainNav/PageMainNav.vue';
 
 @Component({
     name: 'PageHeader',
     components: {
+        CompactSelector,
         PageNav,
         PageMainNav,
     }
 })
 class PageHeader extends Vue {
+    //
+    // Computed
+    //
+    get isStaffUser(): boolean {
+        return this.$store.state.authType === AuthTypes.STAFF;
+    }
 }
 
 export default toNative(PageHeader);
