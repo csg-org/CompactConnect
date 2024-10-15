7b45d41467c08d130a32f138b5615dee227293bc
Landon Shumway <landon@inspiringapps.com>
2024-10-15 17:07:29 -0500

    combine shell scripts into one

--- backend/bin/format_python.sh
+++ backend/bin/format_python.sh
@@ -3,10 +3,14 @@
 # Find all Python files in the backend directory and format them using Ruff
 # if check flag is passed in, run check without actually formatting the files
 if [ "$1" == "--check" ]; then
-    # print that wer'e fixing the files
-    echo "checking format on Python files..."
+    echo "checking lint errors on Python files..."
+    ruff check $(git ls-files '*.py')
     ruff format --check $(git ls-files '*.py')
-# else we just run check
+elif [ "$1" == "--fix" ]; then
+    echo "Fixing all autocorrect errors in Python files..."
+    ruff check --fix $(git ls-files '*.py')
+    ruff format $(git ls-files '*.py')
+# else we just run the formatter
 else
     echo "Formatting Python files..."
     # this step is required to sort the imports
