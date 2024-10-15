d1264e3fdeba2afe0deffaa35a2a428ad7dfd3c2
Landon Shumway <landon@inspiringapps.com>
2024-10-15 09:35:26 -0500

    fix merge conflict

--- backend/compact-connect/lambdas/purchases/data_model/schema/__init__.py
+++ backend/compact-connect/lambdas/purchases/data_model/schema/__init__.py
@@ -1,4 +1,4 @@
-# pylint: disable=import-self
+# ruff: noqa: F401
 # We import all the record types with the package to ensure they are all registered
 from .compact import CompactRecordSchema
 from .jurisdiction import JurisdictionRecordSchema
--- backend/compact-connect/lambdas/purchases/tests/__init__.py
+++ backend/compact-connect/lambdas/purchases/tests/__init__.py
@@ -1,4 +1,3 @@
-import json
 import os
 from unittest import TestCase
 from unittest.mock import MagicMock
