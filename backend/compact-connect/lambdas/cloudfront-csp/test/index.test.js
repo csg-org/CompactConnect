c7bd749d0376a3d036469fda2accd02c85528ff6
Justin Frahm <justin@inspiringapps.com>
2024-10-16 11:30:06 -0600

    Update test

--- backend/compact-connect/lambdas/cloudfront-csp/test/index.test.js
+++ backend/compact-connect/lambdas/cloudfront-csp/test/index.test.js
@@ -49,10 +49,10 @@ const environments = {
         },
         justin: {
             webFrontend: `app.justin.jcc.iaapi.io`,
-            dataApi: `api.test.jcc.iaapi.io`,
-            s3Upload: `test-persistentstack-mockbulkuploadsbucket0e8f27eb-4h1anohxetmp.s3.amazonaws.com`,
-            cognitoStaff: ``,
-            cognitoProvider: ``,
+            dataApi: `api.justin.jcc.iaapi.io`,
+            s3Upload: `sandbox-persistentstack-bulkuploadsbucketda4bdcd0-pi5pskm7prtp.s3.amazonaws.com`,
+            cognitoStaff: `ia-cc-staff-justin.auth.us-east-1.amazoncognito.com`,
+            cognitoProvider: `ia-cc-provider-justin.auth.us-east-1.amazoncognito.com`,
         },
     },
 };
