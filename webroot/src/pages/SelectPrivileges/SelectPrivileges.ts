12682c00c533e7843088d4a5d5e6035ef237527f
Dana Stiefel <dana@inspiringapps.com>
2024-10-16 10:53:11 -0600

    start

---
+++ webroot/src/pages/SelectPrivileges/SelectPrivileges.ts
@@ -0,0 +1,30 @@
+//
+//  SelectPrivileges.ts
+//  CompactConnect
+//
+//  Created by InspiringApps on 10/15/2024.
+//
+
+import { Component, Vue } from 'vue-facing-decorator';
+
+@Component({
+    name: 'SelectPrivileges',
+    components: {}
+})
+export default class SelectPrivileges extends Vue {
+    //
+    // Data
+    //
+
+    //
+    // Lifecycle
+    //
+
+    //
+    // Computed
+    //
+
+    //
+    // Methods
+    //
+}
---
+++ webroot/src/pages/SelectPrivileges/SelectPrivileges.vue
@@ -0,0 +1,13 @@
+<!--
+    SelectPrivileges.vue
+    CompactConnect
+
+    Created by InspiringApps on 10/15/2024.
+-->
+
+<template>
+    <h3>SelectPrivileges</h3>
+</template>
+
+<script lang="ts" src="./SelectPrivileges.ts"></script>
+<style scoped lang="less" src="./SelectPrivileges.less"></style>
