506d589437d9701306fe965060f4fede61506c32
Dana Stiefel <dana@inspiringapps.com>
2024-10-18 16:20:06 -0600

    add missing files

---
+++ webroot/src/models/PrivilegePurchaseOption/PrivilegePurchaseOption.model.ts
@@ -0,0 +1,62 @@
+//
+//  License.ts
+//  CompactConnect
+//
+//  Created by InspiringApps on 7/2/2024.
+//
+import { FeeTypes } from '@/app.config';
+import deleteUndefinedProperties from '@models/_helpers';
+import { State } from '@models/State/State.model';
+
+// ========================================================
+// =                       Interface                      =
+// ========================================================
+export interface InterfacePrivilegePurchaseOption {
+    jurisdiction?: State | null;
+    compact?: string | null;
+    fee?: number | null;
+    feeType?: FeeTypes | null;
+    isMilitaryDiscountActive?: boolean;
+    militaryDiscountType?: FeeTypes | null;
+    militaryDiscountAmount?: number | null;
+    isJurisprudenceRequired?: boolean;
+}
+
+// ========================================================
+// =                        Model                         =
+// ========================================================
+export class PrivilegePurchaseOption implements InterfacePrivilegePurchaseOption {
+    public $tm?: any = () => [];
+    public jurisdiction? = null;
+
+    constructor(data?: InterfacePrivilegePurchaseOption) {
+        const cleanDataObject = deleteUndefinedProperties(data);
+        const global = window as any;
+        const $tm = global.Vue?.config?.globalProperties?.$tm;
+
+        if ($tm) {
+            this.$tm = $tm;
+        }
+
+        Object.assign(this, cleanDataObject);
+    }
+}
+
+// ========================================================
+// =                      Serializer                      =
+// ========================================================
+export class PrivilegePurchaseOptionSerializer {
+    static fromServer(json: any): PrivilegePurchaseOption {
+        const purchaseOptionData = {
+            jurisdiction: new State({ abbrev: json.postalAbbreviation }),
+            compact: json.compact,
+            fee: json.jurisdictionFee,
+            isMilitaryDiscountActive: json?.militaryDiscount?.acvtive === true || false,
+            militaryDiscountType: json?.militaryDiscount?.discountType || null,
+            militaryDiscountAmount: json?.militaryDiscount?.discountAmount || null,
+            isJurisprudenceRequired: json?.jurisprudenceRequirements?.required || false,
+        };
+
+        return new PrivilegePurchaseOption(purchaseOptionData);
+    }
+}
