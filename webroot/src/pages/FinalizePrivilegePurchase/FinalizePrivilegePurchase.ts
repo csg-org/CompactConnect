532a0b8f39270e090c49f2c1049fc4cd7c7b000b
Dana Stiefel <dana@inspiringapps.com>
2024-10-28 17:14:10 -0600

    work

--- webroot/src/pages/FinalizePrivilegePurchase/FinalizePrivilegePurchase.ts
+++ webroot/src/pages/FinalizePrivilegePurchase/FinalizePrivilegePurchase.ts
@@ -5,9 +5,10 @@
 //  Created by InspiringApps on 10/28/2024.
 //
 
-import { Component, mixins } from 'vue-facing-decorator';
+import { Component, mixins, Vue } from 'vue-facing-decorator';
 import { reactive, computed } from 'vue';
 import MixinForm from '@components/Forms/_mixins/form.mixin';
+import InputButton from '@components/Forms/InputButton/InputButton.vue';
 import InputText from '@components/Forms/InputText/InputText.vue';
 import InputSelect from '@components/Forms/InputSelect/InputSelect.vue';
 import InputCheckbox from '@components/Forms/InputCheckbox/InputCheckbox.vue';
@@ -16,7 +17,13 @@ import InputSubmit from '@components/Forms/InputSubmit/InputSubmit.vue';
 
 @Component({
     name: 'FinalizePrivilegePurchase',
-    components: {}
+    components: {
+        InputText,
+        InputSelect,
+        InputCheckbox,
+        InputSubmit,
+        InputButton
+    }
 })
 export default class FinalizePrivilegePurchase extends mixins(MixinForm) {
     //
--- webroot/src/pages/FinalizePrivilegePurchase/FinalizePrivilegePurchase.vue
+++ webroot/src/pages/FinalizePrivilegePurchase/FinalizePrivilegePurchase.vue
@@ -26,83 +26,13 @@
                     <InputSubmit
                         :formInput="formData.submit"
                         :label="submitLabel"
-                        :isEnabled="!isFormLoading && isAtLeastOnePrivilegeChosen"
+                        :isEnabled="!isFormLoading"
                     />
                 </div>
             </div>
             <div class="finalize-purchase-core-container">
                 <div class="finalize-purchase-title">
                     {{selectPrivilegesTitleText}}
-                </div>
-                <div class="lists-container">
-                    <ul class="state-select-list">
-                        <li
-                            v-for="state in stateCheckList"
-                            :key="state.label"
-                        >
-                            <div v-if="checkIfStateSelectIsDisabled(state)" class="state-select-unit">
-                                <div class="disabled-state-overlay" />
-                                <InputCheckbox
-                                    :formInput="state"
-                                />
-                            </div>
-                            <div
-                                v-else
-                                class="state-select-unit"
-                                @click.stop="checkState(state)"
-                                @keyup.enter="checkState(state)"
-                                tabindex="0"
-                            >
-                                <InputCheckbox
-                                    :formInput="state"
-                                    @change="handleStateClicked(state)"
-                                />
-                            </div>
-                        </li>
-                    </ul>
-                    <ul class="selected-state-list">
-                        <li
-                            v-for="(state, i) in selectedStatePurchaseDataList"
-                            :key="state.jurisdiction.abbrev"
-                            class="selected-state-block"
-                        >
-                            <div class="info-row">
-                                <div class="state-title">{{state.jurisdiction.name()}}</div>
-                                <InputButton
-                                    label="X"
-                                    :isTextLike="true"
-                                    aria-label="deselect state"
-                                    @click="deselectState(state)"
-                                />
-                            </div>
-                            <div class="info-row sub-row">
-                                <div class="info-row-label">{{expirationDateText}}</div>
-                                <div class="expire-date-value">{{activeLicenseExpirationDate}}</div>
-                            </div>
-                            <div class="info-row sub-row">
-                                <div class="info-row-label">{{jurisdictionFeeText}}</div>
-                                <div class="expire-date-value">${{state.fee.toFixed(2)}}</div>
-                            </div>
-                            <div class="info-row sub-row">
-                                <div class="info-row-label">{{commissionFeeText}}</div>
-                                <div class="expire-date-value">${{currentCompactCommissionFee.toFixed(2)}}</div>
-                            </div>
-                            <div v-if="state.isMilitaryDiscountActive" class="info-row sub-row">
-                                <div class="info-row-label">{{militaryDiscountText}}</div>
-                                <div class="expire-date-value">-${{state.militaryDiscountAmount.toFixed(2)}}</div>
-                            </div>
-                            <div class="info-row">
-                                <div class="info-row-label">{{subtotalText}}</div>
-                                <div class="expire-date-value">${{subTotalList[i].toFixed(2)}}</div>
-                            </div>
-                            <div v-if="state.isJurisprudenceRequired" class="jurisprudence-check-box">
-                                <InputCheckbox
-                                    :formInput="formData.jurisprudenceConfirmations[state.jurisdiction.abbrev]"
-                                    @change="handleJurisprudenceClicked(state)"
-                                />
-                            </div>
-                        </li>
-                    </ul>
                 </div>
             </div>
             <div class="button-row">
