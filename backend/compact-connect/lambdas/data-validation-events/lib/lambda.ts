479b4818dc34de7fb2e8eb4283265d1d2e72dca7
Justin Frahm <justin@inspiringapps.com>
2024-11-13 20:56:48 -0700

    Get jurisdictions from JurisdictionClient

--- backend/compact-connect/lambdas/data-validation-events/lib/lambda.ts
+++ backend/compact-connect/lambdas/data-validation-events/lib/lambda.ts
@@ -1,10 +1,11 @@
 import type { LambdaInterface } from '@aws-lambda-powertools/commons/lib/esm/types';
 import { Logger } from '@aws-lambda-powertools/logger';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { SESClient } from '@aws-sdk/client-ses';
 import { Context, EventBridgeEvent } from 'aws-lambda';
 
 import { EnvironmentVariablesService } from './environment-variables-service';
-import { ILicenseErrorEventRecord } from './models';
+import { JurisdictionClient } from './jurisdiction-client';
 import { ReportEmailer } from './report-emailer';
 import { EventClient } from './event-client';
 
@@ -12,22 +13,24 @@ const environmentVariables = new EnvironmentVariablesService();
 const logger = new Logger({ logLevel: environmentVariables.getLogLevel() });
 
 
-interface LambdaOptions {
-    dynamoDBClient: DynamoDBClient,
+interface LambdaProperties {
+    dynamoDBClient: DynamoDBClient;
+    sesClient: SESClient;
 }
 
 /*
  * Basic Lambda class to integrate the primary lambda entrypoint logic, logging, and error handling
  */
 export class Lambda implements LambdaInterface {
-    private readonly dynamoDBClient: DynamoDBClient;
-    private readonly sesClient: SESClient;
+    private readonly jurisdictionClient: JurisdictionClient;
     private readonly eventClient: EventClient;
     private readonly reportEmailer: ReportEmailer;
 
     constructor(props: LambdaProperties) {
-        this.dynamoDBClient = props.dynamoDBClient;
-        this.sesClient = props.sesClient;
+        this.jurisdictionClient = new JurisdictionClient({
+            logger: logger,
+            dynamoDBClient: props.dynamoDBClient,
+        });
         this.eventClient = new EventClient({
             logger: logger,
             dynamoDBClient: props.dynamoDBClient,
@@ -42,30 +45,39 @@ export class Lambda implements LambdaInterface {
     public async handler(event: EventBridgeEvent<string, any>, context: Context): Promise<any> {
         logger.info('Processing event', { event: event });
 
-        // const resp = await this.dynamoDBClient.send(new ScanCommand({
-        //     TableName: environmentVariables.getDataEventTableName(),
-        //     Select: 'ALL_ATTRIBUTES',
-        // }));
-        //
-        // // Transform the items using unmarshall
-        // const transformedItems = resp.Items?.map((item) => unmarshall(item)) || [];
-
         const [ startTimeStamp, endTimeStamp ] = this.eventClient.getYesterdayTimestamps();
 
-        for (jurisdiction of jurisdictions) {
-            const errors: Promise<ILicenseErrorEventRecord> = this.eventClient.getValidationErrors(
-                compact,
-                jurisdiction,
-                startTimeStamp,
-                endTimeStamp
-            );
-            errors.then(
-                this.reportEmailer.sendReportEmail(error);
-            )
+        for (const compact of environmentVariables.getCompacts()) {
+            const jurisdictions = await this.jurisdictionClient.getJurisdictions(compact);
+
+            for (const jurisdiction of jurisdictions) {
+                const ingestEvents = await this.getEvents(
+                    compact, jurisdiction.postalAbbreviation, startTimeStamp, endTimeStamp
+                );
+
+                const messageId = await this.reportEmailer.sendReportEmail(
+                    ingestEvents, jurisdiction.jurisdictionOperationsTeamEmails
+                );
+
+                logger.info('Sent email', { compact: compact, jurisdiction: jurisdiction, messageId: messageId });
+            }
         }
 
-        logger.debug('Retrieved records', { items: transformedItems });
+        logger.info('Completing handler');
+    }
 
-        return transformedItems;
+    private async getEvents(compact: string, jurisdiction: string, startTimeStamp: number, endTimeStamp: number) {
+        const validationErrors = await this.eventClient.getValidationErrors(
+            compact, jurisdiction, startTimeStamp, endTimeStamp
+        );
+
+        const ingestFailures = await this.eventClient.getIngestFailures(
+            compact, jurisdiction, startTimeStamp, endTimeStamp
+        );
+
+        return {
+            ingestFailures: ingestFailures,
+            validationErrors: validationErrors,
+        };
     }
 }
