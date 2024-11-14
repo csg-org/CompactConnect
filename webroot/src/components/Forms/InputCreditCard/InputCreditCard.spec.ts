99d0471b2e5cf1871e9bad1ecb0eaf7d72068f11
Dana Stiefel <dana@inspiringapps.com>
2024-11-14 15:57:55 -0700

    add tests for credit card formatting

--- webroot/src/components/Forms/InputCreditCard/InputCreditCard.spec.ts
+++ webroot/src/components/Forms/InputCreditCard/InputCreditCard.spec.ts
@@ -6,8 +6,11 @@
 //
 
 import { expect } from 'chai';
-import { mountShallow } from '@tests/helpers/setup';
+import { mountShallow, mountFull } from '@tests/helpers/setup';
 import InputCreditCard from '@components/Forms/InputCreditCard/InputCreditCard.vue';
+import ShowPasswordEye from '@components/Icons/ShowPasswordEye/ShowPasswordEye.vue';
+import HidePasswordEye from '@components/Icons/HidePasswordEye/HidePasswordEye.vue';
+import { FormInput } from '@models/FormInput/FormInput.model';
 
 describe('InputCreditCard component', async () => {
     it('should mount the component', async () => {
@@ -15,5 +18,23 @@ describe('InputCreditCard component', async () => {
 
         expect(wrapper.exists()).to.equal(true);
         expect(wrapper.findComponent(InputCreditCard).exists()).to.equal(true);
+    });
+    it('should toggle the credit card visibility', async () => {
+        const wrapper = await mountFull(InputCreditCard, {
+            props: {
+                formInput: new FormInput(),
+                showEyeIcon: true,
+            },
+        });
+        const eyeIcon = wrapper.find('.eye-icon-container');
+        const input = wrapper.find('input');
+
+        await eyeIcon.trigger('click');
+        expect(input.attributes().type).to.equal('text');
+        expect(wrapper.findComponent(ShowPasswordEye).exists()).to.equal(true);
+
+        await eyeIcon.trigger('click');
+        expect(input.attributes().type).to.equal('password');
+        expect(wrapper.findComponent(HidePasswordEye).exists()).to.equal(true);
     });
 });
