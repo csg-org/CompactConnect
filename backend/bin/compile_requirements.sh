3c9dfdbacc397cacdf48eb8e1a4a98017b8b5b44
Landon Shumway <landon@inspiringapps.com>
2024-11-15 13:46:26 -0600

    Remove license-data and delete-objects directories

--- backend/bin/compile_requirements.sh
+++ backend/bin/compile_requirements.sh
@@ -8,8 +8,6 @@ pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/common-python/
 pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/common-python/requirements-dev.in
 pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/custom-resources/requirements.in
 pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/custom-resources/requirements-dev.in
-pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/license-data/requirements.in
-pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/license-data/requirements-dev.in
 pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/provider-data-v1/requirements.in
 pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/provider-data-v1/requirements-dev.in
 pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/purchases/requirements.in
@@ -18,6 +16,4 @@ pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/staff-user-pre
 pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/staff-user-pre-token/requirements-dev.in
 pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/staff-users/requirements.in
 pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/staff-users/requirements-dev.in
-pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/delete-objects/requirements.in
-pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/delete-objects/requirements-dev.in
 bin/sync_deps.sh
