7742944b64a31e3761a90e2e868da4579d03cf12
Landon Shumway <landon@inspiringapps.com>
2024-11-15 13:46:26 -0600

    Remove license-data and delete-objects directories

--- backend/bin/sync_deps.sh
+++ backend/bin/sync_deps.sh
@@ -3,16 +3,12 @@ pip-sync \
   multi-account/requirements-dev.txt \
   compact-connect/requirements.txt \
   compact-connect/requirements-dev.txt \
-  compact-connect/lambdas/license-data/requirements.txt \
-  compact-connect/lambdas/license-data/requirements-dev.txt \
   compact-connect/lambdas/provider-data-v1/requirements.txt \
   compact-connect/lambdas/provider-data-v1/requirements-dev.txt \
   compact-connect/lambdas/staff-user-pre-token/requirements.txt \
   compact-connect/lambdas/staff-user-pre-token/requirements-dev.txt \
   compact-connect/lambdas/staff-users/requirements.txt \
   compact-connect/lambdas/staff-users/requirements-dev.txt \
-  compact-connect/lambdas/delete-objects/requirements.txt \
-  compact-connect/lambdas/delete-objects/requirements-dev.txt \
   compact-connect/lambdas/custom-resources/requirements.txt \
   compact-connect/lambdas/custom-resources/requirements-dev.txt \
   compact-connect/lambdas/purchases/requirements.txt \
