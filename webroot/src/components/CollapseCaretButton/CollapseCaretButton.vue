29bedd773408e5b20b74b0162a1f5a58a98ad895
Dana Stiefel <dana@inspiringapps.com>
2024-11-21 11:16:02 -0700

    finish fixes

--- webroot/src/components/CollapseCaretButton/CollapseCaretButton.vue
+++ webroot/src/components/CollapseCaretButton/CollapseCaretButton.vue
@@ -13,14 +13,14 @@
         class="collapse-caret-button"
     >
         <svg
-            v-if="isUp" width="24" height="14" viewBox="0 0 24 14" role="img"
-            fill="none" xmlns="http://www.w3.org/2000/svg">
+            v-if="isUp" viewBox="0 0 24 14" role="img"
+            fill="none">
             <title>Collapse Button Logo</title>
             <path d="M2 11.6342L11.9054 1.72852L21.8111 11.6342"
             stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
-        <svg v-else width="24" height="14"
-            viewBox="0 0 24 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg v-else
+            viewBox="0 0 24 14" fill="none">
             <title>Expand Button Logo</title>
             <path d="M21.811 1.7271L11.9056 11.6328L1.99991 1.7271"
             stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
