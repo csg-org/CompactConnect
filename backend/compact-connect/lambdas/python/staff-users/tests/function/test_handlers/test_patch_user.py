8df21455c2a9fa43ae92dca48f997bcc5bd2d15b
Landon Shumway <landon@inspiringapps.com>
2024-12-12 10:59:01 -0600

    create/patch staff users account for 'readPrivate' permission

--- backend/compact-connect/lambdas/python/staff-users/tests/function/test_handlers/test_patch_user.py
+++ backend/compact-connect/lambdas/python/staff-users/tests/function/test_handlers/test_patch_user.py
@@ -121,3 +121,38 @@ class TestPatchUser(TstFunction):
         resp = patch_user(event, self.mock_context)
 
         self.assertEqual(403, resp['statusCode'])
+
+    def test_patch_user_allows_adding_read_private_permission(self):
+        self._load_user_data()
+
+        from handlers.users import patch_user
+
+        with open('tests/resources/api-event.json') as f:
+            event = json.load(f)
+
+        # The user has admin permission for aslp/oh
+        event['requestContext']['authorizer']['claims']['scope'] = 'openid email aslp/admin aslp/oh.admin aslp/aslp.admin'
+        event['pathParameters'] = {'compact': 'aslp', 'userId': 'a4182428-d061-701c-82e5-a3d1d547d797'}
+        event['body'] = json.dumps({
+            'permissions': {'aslp': {"actions": {'readPrivate': True,},
+                                     'jurisdictions': {'oh': {'actions': {'admin': True, 'readPrivate': True}}}}}})
+
+        resp = patch_user(event, self.mock_context)
+
+        self.assertEqual(200, resp['statusCode'])
+        user = json.loads(resp['body'])
+        self.assertEqual(
+            {
+                'attributes': {'email': 'justin@example.org', 'familyName': 'Williams', 'givenName': 'Justin'},
+                'dateOfUpdate': '2024-09-12T23:59:59+00:00',
+                'permissions': {
+                    'aslp': {
+                        'actions': {'readPrivate': True},
+                        'jurisdictions': {'oh': {'actions': {'admin': True, 'write': True, 'readPrivate': True}}},
+                    },
+                },
+                'type': 'user',
+                'userId': 'a4182428-d061-701c-82e5-a3d1d547d797',
+            },
+            user,
+        )
