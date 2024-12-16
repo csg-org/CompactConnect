ebb0546ffa98bb244f8e051e319fb95373107ed1
Landon Shumway <landon@inspiringapps.com>
2024-12-16 17:01:27 -0600

    formatter changes

--- backend/compact-connect/tests/smoke/query_provider_smoke_tests.py
+++ backend/compact-connect/tests/smoke/query_provider_smoke_tests.py
@@ -20,6 +20,7 @@ from tests.smoke.smoke_common import SmokeTestFailureException, call_provider_us
 # To run this script, create a smoke_tests_env.json file in the same directory as this script using the
 # 'smoke_tests_env_example.json' file as a template.
 
+
 def get_provider_user_smoke_test():
     """
     Verifies that a provider record can be fetched from the GET provider users endpoint of the Compact Connect API.
@@ -39,7 +40,7 @@ def get_provider_user_smoke_test():
     get_provider_response = requests.get(
         url=get_api_base_url() + f'/v1/compacts/{provider_compact}/providers/{provider_id}',
         headers=staff_users_headers,
-        timeout=10
+        timeout=10,
     )
 
     if get_provider_response.status_code != 200:
@@ -61,10 +62,13 @@ def get_provider_user_smoke_test():
         military_affiliation.pop('documentKeys', None)
 
     if provider_object != provider_user_profile:
-        raise SmokeTestFailureException(f'Provider object does not match the profile.\n'
-                                        f'Profile response: {json.dumps(provider_user_profile)}\n'
-                                        f'Get Provider response: {json.dumps(provider_object)}')
+        raise SmokeTestFailureException(
+            f'Provider object does not match the profile.\n'
+            f'Profile response: {json.dumps(provider_user_profile)}\n'
+            f'Get Provider response: {json.dumps(provider_object)}'
+        )
     print('Successfully fetched expected provider records.')
+
 
 def query_provider_user_smoke_test():
     """
@@ -83,15 +87,13 @@ def query_provider_user_smoke_test():
 
     # Step 2: Have the staff user query for that provider using the profile information.
     staff_users_headers = get_staff_user_auth_headers()
-    post_body = {
-        'query': {'providerId': provider_id}
-    }
+    post_body = {'query': {'providerId': provider_id}}
 
     post_response = requests.post(
         url=get_api_base_url() + f'/v1/compacts/{provider_compact}/providers/query',
         headers=staff_users_headers,
         json=post_body,
-        timeout=10
+        timeout=10,
     )
 
     if post_response.status_code != 200:
@@ -111,13 +113,13 @@ def query_provider_user_smoke_test():
     provider_user_profile.pop('privileges')
 
     if provider_object != provider_user_profile:
-        raise SmokeTestFailureException(f'Provider object does not match the profile.\n'
-                                        f'Profile response: {provider_user_profile}\n'
-                                        f'Query Provider object: {provider_object}')
+        raise SmokeTestFailureException(
+            f'Provider object does not match the profile.\n'
+            f'Profile response: {provider_user_profile}\n'
+            f'Query Provider object: {provider_object}'
+        )
 
     print('Successfully queried expected provider record.')
-
-
 
 
 if __name__ == '__main__':
