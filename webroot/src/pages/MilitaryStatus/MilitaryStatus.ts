3872d12ed4474a2fd776dfad37a5482b7858a8e3
Dana Stiefel <dana@inspiringapps.com>
2024-12-30 00:13:14 -0700

    some military status styling

--- webroot/src/pages/MilitaryStatus/MilitaryStatus.ts
+++ webroot/src/pages/MilitaryStatus/MilitaryStatus.ts
@@ -7,6 +7,7 @@
 
 import { Component, Vue } from 'vue-facing-decorator';
 import InputButton from '@components/Forms/InputButton/InputButton.vue';
+import { Compact } from '@models/Compact/Compact.model';
 
 @Component({
     name: 'MilitaryStatus',
@@ -26,11 +27,55 @@ export default class MilitaryStatus extends Vue {
     //
     // Computed
     //
+    get statusTitleText(): string {
+        return this.$t('licensing.status').toUpperCase();
+    }
+
+    get status(): string {
+        return 'Active';
+    }
+
+    get affiliationTypeTitle(): string {
+        return this.$t('military.affiliationType').toUpperCase();
+    }
+
+    get affiliationType(): string {
+        return 'Active-duty military member';
+    }
+
+    get previouslyUploadedTitle(): string {
+        return this.$t('military.previouslyUploadedDocuments').toUpperCase();
+    }
+
+    get currentCompact(): Compact | null {
+        return this.userStore?.currentCompact || null;
+    }
+
+    get currentCompactType(): string | null {
+        return this.currentCompact?.type || null;
+    }
+
+    get userStore(): any {
+        return this.$store.state.user;
+    }
 
     //
     // Methods
     //
     goBack() {
         console.log('go back');
+    }
+
+    endAffiliation() {
+        console.log('ending');
+    }
+
+    editInfo() {
+        if (this.currentCompactType) {
+            this.$router.push({
+                name: 'UpdateMilitaryStatus',
+                params: { compact: this.currentCompactType }
+            });
+        }
     }
 }
--- webroot/src/pages/MilitaryStatus/MilitaryStatus.vue
+++ webroot/src/pages/MilitaryStatus/MilitaryStatus.vue
@@ -11,6 +11,7 @@
             :label="$t('common.back')"
             :aria-label="$t('common.back')"
             :isTextLike="true"
+            :shouldHideMargin="true"
             class="back-btn"
             @click="goBack"
         />
@@ -18,14 +19,36 @@
             {{ $t('military.militaryStatusTitle') }}
         </div>
         <div class="core-info-block">
-            <div class="status-chunk">
-                <div class="chunk-title"></div>
+            <div class="info-row">
+                <div class="chunk">
+                    <div class="chunk-title">{{statusTitleText}}</div>
+                    <div class="chunk-text">{{status}}</div>
+                </div>
+                <div class="chunk">
+                    <div class="chunk-title">{{affiliationTypeTitle}}</div>
+                    <div class="chunk-text">{{affiliationType}}</div>
+                </div>
             </div>
-            <div class="type-chunk">
-                <div class="chunk-title"></div>
+            <div class="chunk">
+                <div class="chunk-title">{{previouslyUploadedTitle}}</div>
+                <div class="prev-doc-table"></div>
             </div>
-            <div class="table-chunk">
-                <div class="chunk-title"></div>
+            <div class="button-row">
+                <InputButton
+                    :label="$t('military.endMilitaryAffiliation')"
+                    :aria-label="$t('military.endMilitaryAffiliation')"
+                    :isTextLike="true"
+                    :shouldHideMargin="true"
+                    class="end-aff-button"
+                    @click="endAffiliation"
+                />
+                <InputButton
+                    :label="$t('military.editInfo')"
+                    :aria-label="$t('military.editInfo')"
+                    :shouldHideMargin="true"
+                    class="edit-info-button"
+                    @click="editInfo"
+                />
             </div>
         </div>
    </div>
