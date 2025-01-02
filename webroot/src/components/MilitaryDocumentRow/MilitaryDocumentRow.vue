16fe11ece83522dd717d66d8de65cd8b5f9028c6
Dana Stiefel <dana@inspiringapps.com>
2025-01-02 15:40:37 -0700

    work

---
+++ webroot/src/components/MilitaryDocumentRow/MilitaryDocumentRow.vue
@@ -0,0 +1,20 @@
+<!--
+    MilitaryDocumentRow.vue
+    InspiringApps modules
+
+    Created by InspiringApps on 4/30/2021.
+-->
+
+<template>
+    <li class="example-row">
+        <div class="cell name" :class="{ 'is-header': isHeaderRow }">
+            {{ item.name }}
+        </div>
+        <div class="cell size" :class="{ 'is-header': isHeaderRow }">
+            {{ item.date }}
+        </div>
+    </li>
+</template>
+
+<script lang="ts" src="./MilitaryDocumentRow.ts"></script>
+<style scoped lang="less" src="./MilitaryDocumentRow.less"></style>
