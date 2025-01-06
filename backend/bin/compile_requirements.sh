be20c8b9aeedb67522ef8098ec3cc3768c422583
Landon Shumway <landon@inspiringapps.com>
2025-01-06 10:29:16 -0600

    update requirements/address pip-compile warning
    pip-compile was issuing this warning:
    WARNING: --strip-extras is becoming the default in version 8.0.0. To silence this warning, either use --strip-extras to opt into the new default or use --no-strip-extras to retain the existing behavior.

--- backend/bin/compile_requirements.sh
+++ backend/bin/compile_requirements.sh
@@ -1,23 +1,23 @@
 set -e
 
-pip-compile --no-emit-index-url --upgrade multi-account/requirements-dev.in
-pip-compile --no-emit-index-url --upgrade multi-account/requirements.in
-pip-compile --no-emit-index-url --upgrade compact-connect/requirements-dev.in
-pip-compile --no-emit-index-url --upgrade compact-connect/requirements.in
-pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/python/attestations/requirements-dev.in
-pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/python/attestations/requirements.in
-pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/python/common/requirements-dev.in
-pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/python/common/requirements.in
-pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/python/custom-resources/requirements-dev.in
-pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/python/custom-resources/requirements.in
-pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/python/data-events/requirements-dev.in
-pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/python/data-events/requirements.in
-pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/python/provider-data-v1/requirements-dev.in
-pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/python/provider-data-v1/requirements.in
-pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/python/purchases/requirements-dev.in
-pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/python/purchases/requirements.in
-pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/python/staff-user-pre-token/requirements-dev.in
-pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/python/staff-user-pre-token/requirements.in
-pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/python/staff-users/requirements-dev.in
-pip-compile --no-emit-index-url --upgrade compact-connect/lambdas/python/staff-users/requirements.in
+pip-compile --no-emit-index-url --upgrade --no-strip-extras multi-account/requirements-dev.in
+pip-compile --no-emit-index-url --upgrade --no-strip-extras multi-account/requirements.in
+pip-compile --no-emit-index-url --upgrade --no-strip-extras compact-connect/requirements-dev.in
+pip-compile --no-emit-index-url --upgrade --no-strip-extras compact-connect/requirements.in
+pip-compile --no-emit-index-url --upgrade --no-strip-extras compact-connect/lambdas/python/attestations/requirements-dev.in
+pip-compile --no-emit-index-url --upgrade --no-strip-extras compact-connect/lambdas/python/attestations/requirements.in
+pip-compile --no-emit-index-url --upgrade --no-strip-extras compact-connect/lambdas/python/common/requirements-dev.in
+pip-compile --no-emit-index-url --upgrade --no-strip-extras compact-connect/lambdas/python/common/requirements.in
+pip-compile --no-emit-index-url --upgrade --no-strip-extras compact-connect/lambdas/python/custom-resources/requirements-dev.in
+pip-compile --no-emit-index-url --upgrade --no-strip-extras compact-connect/lambdas/python/custom-resources/requirements.in
+pip-compile --no-emit-index-url --upgrade --no-strip-extras compact-connect/lambdas/python/data-events/requirements-dev.in
+pip-compile --no-emit-index-url --upgrade --no-strip-extras compact-connect/lambdas/python/data-events/requirements.in
+pip-compile --no-emit-index-url --upgrade --no-strip-extras compact-connect/lambdas/python/provider-data-v1/requirements-dev.in
+pip-compile --no-emit-index-url --upgrade --no-strip-extras compact-connect/lambdas/python/provider-data-v1/requirements.in
+pip-compile --no-emit-index-url --upgrade --no-strip-extras compact-connect/lambdas/python/purchases/requirements-dev.in
+pip-compile --no-emit-index-url --upgrade --no-strip-extras compact-connect/lambdas/python/purchases/requirements.in
+pip-compile --no-emit-index-url --upgrade --no-strip-extras compact-connect/lambdas/python/staff-user-pre-token/requirements-dev.in
+pip-compile --no-emit-index-url --upgrade --no-strip-extras compact-connect/lambdas/python/staff-user-pre-token/requirements.in
+pip-compile --no-emit-index-url --upgrade --no-strip-extras compact-connect/lambdas/python/staff-users/requirements-dev.in
+pip-compile --no-emit-index-url --upgrade --no-strip-extras compact-connect/lambdas/python/staff-users/requirements.in
 bin/sync_deps.sh
