498e65ff69a09ce9a96a14e7ae9d392b31c43601
John Sandoval <john@inspiringapps.com>
2025-02-20 14:42:50 -0700

    WIP: privilege deactivation

--- webroot/src/network/licenseApi/data.api.ts
+++ webroot/src/network/licenseApi/data.api.ts
@@ -234,7 +234,7 @@ export class LicenseDataApi implements DataApiInterface {
      * @return {Promise<object>}                 The server response.
      */
     public async deletePrivilege(compact: string, licenseeId: string, privilegeState: string, licenseType: string) {
-        const serverResponse: any = await this.api.get(`/v1/compacts/${compact}/providers/${licenseeId}/privileges/jurisdiction/${privilegeState}/licenseType/${licenseType}`);
+        const serverResponse: any = await this.api.post(`/v1/compacts/${compact}/providers/${licenseeId}/privileges/jurisdiction/${privilegeState}/licenseType/${licenseType}/deactivate`, {});
 
         return serverResponse;
     }
