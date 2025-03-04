7e0e16bf14a40975785b9d5f884521d3a48fb8ab
Dana Stiefel <dana@inspiringapps.com>
2025-03-04 09:32:26 -0700

    add files

---
+++ webroot/src/components/PrivilegePurchaseLicense/PrivilegePurchaseLicense.ts
@@ -0,0 +1,35 @@
+//
+//  PrivilegePurchaseLicense.ts
+//  CompactConnect
+//
+//  Created by InspiringApps on 3/3/2025.
+//
+
+import { Component, Vue, toNative } from 'vue-facing-decorator';
+
+@Component({
+    name: 'PrivilegePurchaseLicense',
+})
+class PrivilegePurchaseLicense extends Vue {
+    // PROPS
+
+    //
+    // Data
+    //
+
+    //
+    // Lifecycle
+    //
+
+    //
+    // Computed
+    //
+
+    //
+    // Methods
+    //
+}
+
+export default toNative(PrivilegePurchaseLicense);
+
+// export default PrivilegePurchaseLicense;
---
+++ webroot/src/components/PrivilegePurchaseLicense/PrivilegePurchaseLicense.vue
@@ -0,0 +1,13 @@
+<!--
+    PrivilegePurchaseLicense.vue
+    CompactConnect
+
+    Created by InspiringApps on 3/3/2025.
+-->
+
+<template>
+    <h3>PrivilegePurchaseLicense</h3>
+</template>
+
+<script lang="ts" src="./PrivilegePurchaseLicense.ts"></script>
+<style scoped lang="less" src="./PrivilegePurchaseLicense.less"></style>
