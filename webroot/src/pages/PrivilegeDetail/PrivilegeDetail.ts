493bc17351129a7e21ea61b10d9c1fdb0281978a
Dana Stiefel <dana@inspiringapps.com>
2025-03-19 13:18:21 -0600

    work

---
+++ webroot/src/pages/PrivilegeDetail/PrivilegeDetail.ts
@@ -0,0 +1,30 @@
+//
+//  PrivilegeDetail.ts
+//  CompactConnect
+//
+//  Created by InspiringApps on 3/18/2025.
+//
+
+import { Component, Vue } from 'vue-facing-decorator';
+
+@Component({
+    name: 'PrivilegeDetail',
+    components: {}
+})
+export default class PrivilegeDetail extends Vue {
+    //
+    // Data
+    //
+
+    //
+    // Lifecycle
+    //
+
+    //
+    // Computed
+    //
+
+    //
+    // Methods
+    //
+}
---
+++ webroot/src/pages/PrivilegeDetail/PrivilegeDetail.vue
@@ -0,0 +1,13 @@
+<!--
+    PrivilegeDetail.vue
+    CompactConnect
+
+    Created by InspiringApps on 3/18/2025.
+-->
+
+<template>
+    <h3>PrivilegeDetail</h3>
+</template>
+
+<script lang="ts" src="./PrivilegeDetail.ts"></script>
+<style scoped lang="less" src="./PrivilegeDetail.less"></style>
