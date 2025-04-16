InspiringApps/JCC-CompactConnect

deploy UI through CDK

backend/compact-connect/common_constructs/ui_app_config_utility.py
@@ -1,0 +1,161 @@
+import json
+from typing import Optional
+from aws_cdk import Stack
+from aws_cdk.aws_ssm import StringParameter
+from constructs import Construct
+
+
+UI_APP_CONFIGURATION_PARAMETER_NAME = '/deployment/s3/ui_app_configuration'
+
+
+class UIAppConfigUtility:
+    """
+    Utility class for managing UI application configuration in SSM Parameter Store.
+    
+    This class provides helper methods for generating and storing configuration
+    values that need to be shared between the Persistent stack and UI stack.
+    """
+    
+    def __init__(self):
+        self._config: dict[str, str] = {}
+        
+    def set_staff_cognito_values(self, domain_name: str, client_id: str) -> None:
+        """
+        Set Cognito configuration values for staff users.
+        
+        :param domain_name: The Cognito domain name for staff users
+        :param client_id: The UI client ID for staff users
+        """
+        self._config['staff_cognito_domain'] = domain_name
+        self._config['staff_cognito_client_id'] = client_id
+        
+    def set_provider_cognito_values(self, domain_name: str, client_id: str) -> None:
+        """
+        Set Cognito configuration values for provider users.
+        
+        :param domain_name: The Cognito domain name for provider users
+        :param client_id: The UI client ID for provider users
+        """
+        self._config['provider_cognito_domain'] = domain_name
+        self._config['provider_cognito_client_id'] = client_id
+    
+    def set_domain_names(self, ui_domain_name: str, api_domain_name: str) -> None:
+        """
+        Set UI and API domain names.
+        
+        :param ui_domain_name: The domain name for the UI application
+        :param api_domain_name: The domain name for the API
+        """
+        self._config['ui_domain_name'] = ui_domain_name
+        self._config['api_domain_name'] = api_domain_name
+    
+    def get_config_json(self) -> str:
+        """
+        Generate JSON string representation of the configuration.
+        
+        :return: A JSON string containing all configuration values
+        """
+        return json.dumps(self._config)
+    
+    def generate_ssm_parameter(self, scope: Construct, id: str) -> StringParameter:
+        """
+        Create an SSM Parameter with the current configuration.
+        
+        :param scope: The CDK construct scope
+        :param id: The ID for the SSM Parameter construct
+            
+        :return: The created StringParameter construct
+        """
+        return StringParameter(
+            scope,
+            id,
+            parameter_name=UI_APP_CONFIGURATION_PARAMETER_NAME,
+            string_value=self.get_config_json(),
+            description="UI application configuration values",
+        )
+
+
+class UIAppConfigValues:
+    """
+    Class to access UI application configuration values loaded from SSM.
+    """
+
+    def __init__(self, config_json: str):
+        """
+        Initialize with configuration JSON from SSM.
+
+        :param config_json: JSON string containing configuration values
+        """
+        if not config_json:
+            raise ValueError("UI App Configuration Parameter is required. If this parameter is not found, the UI stack will not be deployed.")
+
+        self._config: dict[str, str] = json.loads(config_json)
+
+
+    @staticmethod
+    def load_from_ssm_parameter(stack: Stack) -> Optional['UIAppConfigValues']:
+        """
+        Load configuration values from an existing SSM Parameter.
+
+        :param stack: The CDK stack
+
+        :return: An instance of UIAppConfigValues with loaded configuration if the parameter exists, otherwise None
+        """
+        config_value = StringParameter.value_from_lookup(stack, UI_APP_CONFIGURATION_PARAMETER_NAME, default_value=None)
+        # The first time synth is run, CDK returns a dummy value without actually looking up the value
+        # the second time, it will either return a value if the parameter exists, or None. So we check for both of
+        # those cases here.
+        if config_value is not None and config_value != f'dummy-value-for-{UI_APP_CONFIGURATION_PARAMETER_NAME}':
+            return UIAppConfigValues(config_value)
+
+        return None
+
+    @staticmethod
+    def create_dummy_values() -> 'UIAppConfigValues':
+        """
+        Create a mock instance with default values for testing.
+
+        This method is intended for use where bundling is not required (ie unit tests) and we just populate the config
+        with dummy values.
+
+        :return: An instance of UIAppConfigValues with default test values
+        """
+        test_config = {
+            'staff_cognito_domain': 'test-staff-domain',
+            'staff_cognito_client_id': 'test-staff-client-id',
+            'provider_cognito_domain': 'test-provider-domain',
+            'provider_cognito_client_id': 'test-provider-client-id',
+            'ui_domain_name': 'test-ui.example.com',
+            'api_domain_name': 'test-api.example.com'
+        }
+        return UIAppConfigValues(json.dumps(test_config))
+
+    @property
+    def staff_cognito_domain(self) -> str:
+        """Get the Cognito domain name for staff users."""
+        return self._config['staff_cognito_domain']
+    
+    @property
+    def staff_cognito_client_id(self) -> str:
+        """Get the UI client ID for staff users."""
+        return self._config['staff_cognito_client_id']
+    
+    @property
+    def provider_cognito_domain(self) -> str:
+        """Get the Cognito domain name for provider users."""
+        return self._config['provider_cognito_domain']
+    
+    @property
+    def provider_cognito_client_id(self) -> str:
+        """Get the UI client ID for provider users."""
+        return self._config['provider_cognito_client_id']
+    
+    @property
+    def ui_domain_name(self) -> str:
+        """Get the domain name for the UI application."""
+        return self._config['ui_domain_name']
+    
+    @property
+    def api_domain_name(self) -> str:
+        """Get the domain name for the API."""
+        return self._config['api_domain_name']

backend/compact-connect/stacks/ui_stack/deployment.py
@@ -1,0 +1,112 @@
+# ruff: noqa: E501 line-too-long
+# For the sake of readability, we don't want to break up environment values into separate lines
+import os
+
+from aws_cdk import BundlingOptions, DockerImage, Size, Stack
+from aws_cdk.aws_s3 import IBucket
+from aws_cdk.aws_s3_deployment import BucketDeployment, Source
+from cdk_nag import NagSuppressions
+from common_constructs.ui_app_config_utility import UIAppConfigValues
+from constructs import IConstruct
+
+HTTPS_PREFIX = 'https://'
+COGNITO_AUTH_DOMAIN_SUFFIX = '.auth.us-east-1.amazoncognito.com'
+
+
+class CompactConnectUIBucketDeployment(BucketDeployment):
+    def __init__(
+        self,
+        scope: IConstruct,
+        id: str,
+        *,
+        ui_bucket: IBucket,
+        environment_context: dict,
+        ui_app_config_values: UIAppConfigValues
+    ):
+        stack = Stack.of(scope)
+        # Get environment-specific values from context
+        recaptcha_public_key = environment_context['recaptcha_public_key']
+        robots_meta = environment_context['robots_meta']
+
+        super().__init__(
+            scope,
+            id,
+            destination_bucket=ui_bucket,
+            # Larger memory reserved for the s3 sync process than the default 128MB
+            memory_limit=512,
+            ephemeral_storage_size=Size.mebibytes(1024),
+            sources=[
+                Source.asset(
+                    # we need to back up two parent directories to get to root
+                    os.path.join('..', '..', 'webroot') if stack.bundling_required
+                    else os.path.join('tests', 'resources', 'test_ui_directory'),
+                    # This will take a long time to run because it installs a bunch of packages
+                    # into a docker container before building the UI, every time
+                    # If you aren't working on the UI, you can skip bundling for local dev work
+                    # to save significant time on app synthesis. To do this, set
+                    # cdk-context['aws:cdk:bundling-stacks'] = []
+                    **{
+                        'bundling': BundlingOptions(
+                            # DockerHub was rate-limiting us, so pivot to the AWS public registry
+                            image=DockerImage('public.ecr.aws/lts/ubuntu:22.04_stable'),
+                            environment={
+                                'BASE_URL': '/',
+                                'VUE_APP_DOMAIN': f'{HTTPS_PREFIX}{ui_app_config_values.ui_domain_name}',
+                                'VUE_APP_ROBOTS_META': robots_meta,
+                                'VUE_APP_API_STATE_ROOT': f'{HTTPS_PREFIX}{ui_app_config_values.api_domain_name}',
+                                'VUE_APP_API_LICENSE_ROOT': f'{HTTPS_PREFIX}{ui_app_config_values.api_domain_name}',
+                                'VUE_APP_API_USER_ROOT': f'{HTTPS_PREFIX}{ui_app_config_values.api_domain_name}',
+                                'VUE_APP_COGNITO_REGION': 'us-east-1',
+                                'VUE_APP_COGNITO_AUTH_DOMAIN_STAFF': f'{HTTPS_PREFIX}{ui_app_config_values.staff_cognito_domain}{COGNITO_AUTH_DOMAIN_SUFFIX}',
+                                'VUE_APP_COGNITO_CLIENT_ID_STAFF': ui_app_config_values.staff_cognito_client_id,
+                                'VUE_APP_COGNITO_AUTH_DOMAIN_LICENSEE': f'{HTTPS_PREFIX}{ui_app_config_values.provider_cognito_domain}{COGNITO_AUTH_DOMAIN_SUFFIX}',
+                                'VUE_APP_COGNITO_CLIENT_ID_LICENSEE': ui_app_config_values.provider_cognito_client_id,
+                                'VUE_APP_RECAPTCHA_KEY': recaptcha_public_key,
+                            },
+                            entrypoint=['bash'],
+                            command=['bin/build.sh'],
+                            # Necessary to install system packages
+                            user='root',
+                        )
+                    }
+                    if stack.bundling_required
+                    else {},
+                )
+            ],
+        )
+
+        NagSuppressions.add_resource_suppressions_by_path(
+            stack=stack,
+            path=f'{stack.node.path}/Custom::CDKBucketDeployment8693BB64968944B69AAFB0CC9EB8756C512MiB1024MiB/Resource',
+            suppressions=[
+                {'id': 'AwsSolutions-L1', 'reason': 'This resource is created by CDK. We do not maintain it.'},
+                {
+                    'id': 'HIPAA.Security-LambdaConcurrency',
+                    'reason': 'This lambda is managed by AWS. We do not maintain it.',
+                },
+                {'id': 'HIPAA.Security-LambdaDLQ', 'reason': 'This lambda is managed by AWS. We do not maintain it.'},
+                {
+                    'id': 'HIPAA.Security-LambdaInsideVPC',
+                    'reason': 'This lambda is managed by AWS. We do not maintain it.',
+                },
+            ],
+        )
+
+        NagSuppressions.add_resource_suppressions_by_path(
+            stack=stack,
+            path=f'{stack.node.path}/Custom::CDKBucketDeployment8693BB64968944B69AAFB0CC9EB8756C512MiB1024MiB/ServiceRole/DefaultPolicy/Resource',
+            suppressions=[
+                {'id': 'AwsSolutions-IAM5', 'reason': 'This resource is created by CDK. We do not maintain it.'},
+            ],
+        )
+
+        NagSuppressions.add_resource_suppressions_by_path(
+            stack=stack,
+            path=f'{stack.node.path}/Custom::CDKBucketDeployment8693BB64968944B69AAFB0CC9EB8756C512MiB1024MiB/ServiceRole/Resource',
+            suppressions=[
+                {
+                    'id': 'AwsSolutions-IAM4',
+                    'reason': 'This resource uses AWS managed policies because it is created by CDK. We do not maintain it.',
+                },
+            ],
+        )

csg-org/CompactConnect

backend/compact-connect/common_constructs/ui_app_config_utility.py
@@ -1,0 +1,160 @@
+import json
+from typing import Optional
+
+from aws_cdk import Stack
+from aws_cdk.aws_ssm import StringParameter
+from constructs import Construct
+
+UI_APP_CONFIGURATION_PARAMETER_NAME = '/deployment/s3/ui_app_configuration'
+
+
+class UIAppConfigUtility:
+    """
+    Utility class for managing UI application configuration in SSM Parameter Store.
+
+    This class provides helper methods for generating and storing configuration
+    values that need to be shared between the Persistent stack and UI stack.
+    """
+
+    def __init__(self):
+        self._config: dict[str, str] = {}
+
+    def set_staff_cognito_values(self, domain_name: str, client_id: str) -> None:
+        """
+        Set Cognito configuration values for staff users.
+
+        :param domain_name: The Cognito domain name for staff users
+        :param client_id: The UI client ID for staff users
+        """
+        self._config['staff_cognito_domain'] = domain_name
+        self._config['staff_cognito_client_id'] = client_id
+
+    def set_provider_cognito_values(self, domain_name: str, client_id: str) -> None:
+        """
+        Set Cognito configuration values for provider users.
+
+        :param domain_name: The Cognito domain name for provider users
+        :param client_id: The UI client ID for provider users
+        """
+        self._config['provider_cognito_domain'] = domain_name
+        self._config['provider_cognito_client_id'] = client_id
+
+    def set_domain_names(self, ui_domain_name: str, api_domain_name: str) -> None:
+        """
+        Set UI and API domain names.
+
+        :param ui_domain_name: The domain name for the UI application
+        :param api_domain_name: The domain name for the API
+        """
+        self._config['ui_domain_name'] = ui_domain_name
+        self._config['api_domain_name'] = api_domain_name
+
+    def get_config_json(self) -> str:
+        """
+        Generate JSON string representation of the configuration.
+
+        :return: A JSON string containing all configuration values
+        """
+        return json.dumps(self._config)
+
+    def generate_ssm_parameter(self, scope: Construct, resource_id: str) -> StringParameter:
+        """
+        Create an SSM Parameter with the current configuration.
+
+        :param scope: The CDK construct scope
+        :param resource_id: The ID for the SSM Parameter construct
+
+        :return: The created StringParameter construct
+        """
+        return StringParameter(
+            scope,
+            resource_id,
+            parameter_name=UI_APP_CONFIGURATION_PARAMETER_NAME,
+            string_value=self.get_config_json(),
+            description='UI application configuration values',
+        )
+
+
+class UIAppConfigValues:
+    """
+    Class to access UI application configuration values loaded from SSM.
+    """
+
+    def __init__(self, config_json: str):
+        """
+        Initialize with configuration JSON from SSM.
+
+        :param config_json: JSON string containing configuration values
+        """
+        if not config_json:
+            raise ValueError('UI App Configuration Parameter is required.')
+
+        self._config: dict[str, str] = json.loads(config_json)
+
+    @staticmethod
+    def load_from_ssm_parameter(stack: Stack) -> Optional['UIAppConfigValues']:
+        """
+        Load configuration values from an existing SSM Parameter.
+
+        :param stack: The CDK stack
+
+        :return: An instance of UIAppConfigValues with loaded configuration if the parameter exists, otherwise None
+        """
+        config_value = StringParameter.value_from_lookup(stack, UI_APP_CONFIGURATION_PARAMETER_NAME, default_value=None)
+        # The first time synth is run, CDK returns a dummy value without actually looking up the value
+        # the second time, it will either return a value if the parameter exists, or None. So we check for both of
+        # those cases here.
+        if config_value is not None and config_value != f'dummy-value-for-{UI_APP_CONFIGURATION_PARAMETER_NAME}':
+            return UIAppConfigValues(config_value)
+
+        return None
+
+    @staticmethod
+    def create_dummy_values() -> 'UIAppConfigValues':
+        """
+        Create a mock instance with default values for testing.
+
+        This method is intended for use where bundling is not required (ie unit tests) and we just populate the config
+        with dummy values.
+
+        :return: An instance of UIAppConfigValues with default test values
+        """
+        test_config = {
+            'staff_cognito_domain': 'test-staff-domain',
+            'staff_cognito_client_id': 'test-staff-client-id',
+            'provider_cognito_domain': 'test-provider-domain',
+            'provider_cognito_client_id': 'test-provider-client-id',
+            'ui_domain_name': 'test-ui.example.com',
+            'api_domain_name': 'test-api.example.com',
+        }
+        return UIAppConfigValues(json.dumps(test_config))
+
+    @property
+    def staff_cognito_domain(self) -> str:
+        """Get the Cognito domain name for staff users."""
+        return self._config['staff_cognito_domain']
+
+    @property
+    def staff_cognito_client_id(self) -> str:
+        """Get the UI client ID for staff users."""
+        return self._config['staff_cognito_client_id']
+
+    @property
+    def provider_cognito_domain(self) -> str:
+        """Get the Cognito domain name for provider users."""
+        return self._config['provider_cognito_domain']
+
+    @property
+    def provider_cognito_client_id(self) -> str:
+        """Get the UI client ID for provider users."""
+        return self._config['provider_cognito_client_id']
+
+    @property
+    def ui_domain_name(self) -> str:
+        """Get the domain name for the UI application."""
+        return self._config['ui_domain_name']
+
+    @property
+    def api_domain_name(self) -> str:
+        """Get the domain name for the API."""
+        return self._config['api_domain_name']

backend/compact-connect/stacks/ui_stack/deployment.py
@@ -1,0 +1,113 @@
+# ruff: noqa: E501 line-too-long
+# For the sake of readability, we don't want to break up environment values into separate lines
+import os
+
+from aws_cdk import BundlingOptions, DockerImage, Size, Stack
+from aws_cdk.aws_s3 import IBucket
+from aws_cdk.aws_s3_deployment import BucketDeployment, Source
+from cdk_nag import NagSuppressions
+from common_constructs.ui_app_config_utility import UIAppConfigValues
+from constructs import Construct
+
+HTTPS_PREFIX = 'https://'
+COGNITO_AUTH_DOMAIN_SUFFIX = '.auth.us-east-1.amazoncognito.com'
+
+
+class CompactConnectUIBucketDeployment(BucketDeployment):
+    def __init__(
+        self,
+        scope: Construct,
+        construct_id: str,
+        *,
+        ui_bucket: IBucket,
+        environment_context: dict,
+        ui_app_config_values: UIAppConfigValues,
+    ):
+        stack = Stack.of(scope)
+        # Get environment-specific values from context
+        recaptcha_public_key = environment_context['recaptcha_public_key']
+        robots_meta = environment_context['robots_meta']
+
+        super().__init__(
+            scope,
+            construct_id,
+            destination_bucket=ui_bucket,
+            # Larger memory reserved for the s3 sync process than the default 128MB
+            memory_limit=512,
+            ephemeral_storage_size=Size.mebibytes(1024),
+            sources=[
+                Source.asset(
+                    # we need to back up two parent directories to get to root
+                    os.path.join('..', '..', 'webroot')
+                    if stack.bundling_required
+                    else os.path.join('tests', 'resources', 'test_ui_directory'),
+                    # This will take a long time to run because it installs a bunch of packages
+                    # into a docker container before building the UI, every time
+                    # If you aren't working on the UI, you can skip bundling for local dev work
+                    # to save significant time on app synthesis. To do this, set
+                    # cdk-context['aws:cdk:bundling-stacks'] = []
+                    **{
+                        'bundling': BundlingOptions(
+                            # DockerHub was rate-limiting us, so pivot to the AWS public registry
+                            image=DockerImage('public.ecr.aws/lts/ubuntu:22.04_stable'),
+                            environment={
+                                'BASE_URL': '/',
+                                'VUE_APP_DOMAIN': f'{HTTPS_PREFIX}{ui_app_config_values.ui_domain_name}',
+                                'VUE_APP_ROBOTS_META': robots_meta,
+                                'VUE_APP_API_STATE_ROOT': f'{HTTPS_PREFIX}{ui_app_config_values.api_domain_name}',
+                                'VUE_APP_API_LICENSE_ROOT': f'{HTTPS_PREFIX}{ui_app_config_values.api_domain_name}',
+                                'VUE_APP_API_USER_ROOT': f'{HTTPS_PREFIX}{ui_app_config_values.api_domain_name}',
+                                'VUE_APP_COGNITO_REGION': 'us-east-1',
+                                'VUE_APP_COGNITO_AUTH_DOMAIN_STAFF': f'{HTTPS_PREFIX}{ui_app_config_values.staff_cognito_domain}{COGNITO_AUTH_DOMAIN_SUFFIX}',
+                                'VUE_APP_COGNITO_CLIENT_ID_STAFF': ui_app_config_values.staff_cognito_client_id,
+                                'VUE_APP_COGNITO_AUTH_DOMAIN_LICENSEE': f'{HTTPS_PREFIX}{ui_app_config_values.provider_cognito_domain}{COGNITO_AUTH_DOMAIN_SUFFIX}',
+                                'VUE_APP_COGNITO_CLIENT_ID_LICENSEE': ui_app_config_values.provider_cognito_client_id,
+                                'VUE_APP_RECAPTCHA_KEY': recaptcha_public_key,
+                            },
+                            entrypoint=['bash'],
+                            command=['bin/build.sh'],
+                            # Necessary to install system packages
+                            user='root',
+                        )
+                    }
+                    if stack.bundling_required
+                    else {},
+                )
+            ],
+        )
+
+        NagSuppressions.add_resource_suppressions_by_path(
+            stack=stack,
+            path=f'{stack.node.path}/Custom::CDKBucketDeployment8693BB64968944B69AAFB0CC9EB8756C512MiB1024MiB/Resource',
+            suppressions=[
+                {'id': 'AwsSolutions-L1', 'reason': 'This resource is created by CDK. We do not maintain it.'},
+                {
+                    'id': 'HIPAA.Security-LambdaConcurrency',
+                    'reason': 'This lambda is managed by AWS. We do not maintain it.',
+                },
+                {'id': 'HIPAA.Security-LambdaDLQ', 'reason': 'This lambda is managed by AWS. We do not maintain it.'},
+                {
+                    'id': 'HIPAA.Security-LambdaInsideVPC',
+                    'reason': 'This lambda is managed by AWS. We do not maintain it.',
+                },
+            ],
+        )
+
+        NagSuppressions.add_resource_suppressions_by_path(
+            stack=stack,
+            path=f'{stack.node.path}/Custom::CDKBucketDeployment8693BB64968944B69AAFB0CC9EB8756C512MiB1024MiB/ServiceRole/DefaultPolicy/Resource',
+            suppressions=[
+                {'id': 'AwsSolutions-IAM5', 'reason': 'This resource is created by CDK. We do not maintain it.'},
+            ],
+        )
+
+        NagSuppressions.add_resource_suppressions_by_path(
+            stack=stack,
+            path=f'{stack.node.path}/Custom::CDKBucketDeployment8693BB64968944B69AAFB0CC9EB8756C512MiB1024MiB/ServiceRole/Resource',
+            suppressions=[
+                {
+                    'id': 'AwsSolutions-IAM4',
+                    'reason': 'This resource uses AWS managed policies because it is created by CDK. We do not maintain it.',
+                },
+            ],
+        )