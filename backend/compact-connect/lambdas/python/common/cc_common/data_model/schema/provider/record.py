35b9d805c9593699bc13ba097218ae54839a502d
Landon Shumway <landon@inspiringapps.com>
2025-05-05 20:52:17 -0500

    Add ProviderUpdateRecordSchema for tracking updates to provider data

--- backend/compact-connect/lambdas/python/common/cc_common/data_model/schema/provider/record.py
+++ backend/compact-connect/lambdas/python/common/cc_common/data_model/schema/provider/record.py
@@ -1,11 +1,12 @@
 # ruff: noqa: N801, N815, ARG002  invalid-name unused-argument
 from urllib.parse import quote
 
-from marshmallow import post_load, pre_dump
-from marshmallow.fields import UUID, Date, DateTime, Email, String
+from marshmallow import post_load, pre_dump, post_dump, validates_schema, ValidationError
+from marshmallow.fields import UUID, Date, DateTime, Email, String, Nested, List
 from marshmallow.validate import Length, Regexp
 
-from cc_common.data_model.schema.base_record import BaseRecordSchema, CalculatedStatusRecordSchema
+from cc_common.config import config
+from cc_common.data_model.schema.base_record import BaseRecordSchema, CalculatedStatusRecordSchema, ForgivingSchema
 from cc_common.data_model.schema.fields import (
     ActiveInactive,
     Compact,
@@ -14,8 +15,10 @@ from cc_common.data_model.schema.fields import (
     Jurisdiction,
     LicenseEncumberedStatusField,
     NationalProviderIdentifier,
-    Set, HomeJurisdictionChangeDeactivationStatusField, CurrentHomeJurisdictionField, UNKNOWN_JURISDICTION
+    Set, HomeJurisdictionChangeDeactivationStatusField, CurrentHomeJurisdictionField, UNKNOWN_JURISDICTION,
+    UpdateType,
 )
+from cc_common.data_model.schema.common import ChangeHashMixin
 
 
 @BaseRecordSchema.register_schema('provider')
@@ -113,4 +116,72 @@ class ProviderRecordSchema(CalculatedStatusRecordSchema):
     @post_load
     def drop_fam_giv_mid(self, in_data, **kwargs):  # noqa: ARG001 unused-argument
         del in_data['providerFamGivMid']
+        return in_data
+
+
+class ProviderUpdatePreviousRecordSchema(ForgivingSchema):
+    """
+    A snapshot of a previous state of a provider record
+
+    Serialization direction:
+    DB -> load() -> Python
+    """
+
+    providerId = UUID(required=True, allow_none=False)
+    compact = Compact(required=True, allow_none=False)
+    licenseJurisdiction = Jurisdiction(required=True, allow_none=False)
+    jurisdictionUploadedLicenseStatus = ActiveInactive(required=True, allow_none=False)
+    jurisdictionUploadedCompactEligibility = CompactEligibility(required=True, allow_none=False)
+    encumberedStatus = LicenseEncumberedStatusField(required=False, allow_none=False)
+    ssnLastFour = String(required=True, allow_none=False)
+    npi = NationalProviderIdentifier(required=False, allow_none=False)
+    givenName = String(required=True, allow_none=False, validate=Length(1, 100))
+    middleName = String(required=False, allow_none=False, validate=Length(1, 100))
+    familyName = String(required=True, allow_none=False, validate=Length(1, 100))
+    suffix = String(required=False, allow_none=False, validate=Length(1, 100))
+    dateOfExpiration = Date(required=True, allow_none=False)
+    dateOfBirth = Date(required=True, allow_none=False)
+    homeAddressStreet1 = String(required=True, allow_none=False, validate=Length(2, 100))
+    homeAddressStreet2 = String(required=False, allow_none=False, validate=Length(1, 100))
+    homeAddressCity = String(required=True, allow_none=False, validate=Length(2, 100))
+    homeAddressState = String(required=True, allow_none=False, validate=Length(2, 100))
+    homeAddressPostalCode = String(required=True, allow_none=False, validate=Length(5, 7))
+    emailAddress = Email(required=False, allow_none=False)
+    phoneNumber = ITUTE164PhoneNumber(required=False, allow_none=False)
+    compactConnectRegisteredEmailAddress = Email(required=False, allow_none=False)
+    cognitoSub = String(required=False, allow_none=False)
+    currentHomeJurisdiction = CurrentHomeJurisdictionField(required=False, allow_none=False)
+    dateOfUpdate = DateTime(required=True, allow_none=False)
+
+
+@BaseRecordSchema.register_schema('providerUpdate')
+class ProviderUpdateRecordSchema(BaseRecordSchema, ChangeHashMixin):
+    """
+    Schema for provider update history records in the provider data table
+
+    Serialization direction:
+    DB -> load() -> Python
+    """
+
+    _record_type = 'providerUpdate'
+
+    updateType = UpdateType(required=True, allow_none=False)
+    providerId = UUID(required=True, allow_none=False)
+    compact = Compact(required=True, allow_none=False)
+    previous = Nested(ProviderUpdatePreviousRecordSchema, required=True, allow_none=False)
+    # We'll allow any fields that can show up in the previous field to be here as well, but none are required
+    updatedValues = Nested(ProviderUpdatePreviousRecordSchema(partial=True), required=True, allow_none=False)
+    # List of field names that were present in the previous record but removed in the update
+    removedValues = List(String(), required=False, allow_none=False)
+
+    @post_dump  # Must be _post_ dump so we have values that are more easily hashed
+    def generate_pk_sk(self, in_data, **kwargs):  # noqa: ARG001 unused-argument
+        in_data['pk'] = f'{in_data["compact"]}#PROVIDER#{in_data["providerId"]}'
+        # This needs to include a POSIX timestamp (seconds) and a hash of the changes
+        # to the record. We'll use the current time and the hash of the updatedValues
+        # field for this.
+        change_hash = self.hash_changes(in_data)
+        in_data['sk'] = (
+            f'{in_data["compact"]}#PROVIDER#UPDATE#{int(config.current_standard_datetime.timestamp())}/{change_hash}'
+        )
         return in_data
