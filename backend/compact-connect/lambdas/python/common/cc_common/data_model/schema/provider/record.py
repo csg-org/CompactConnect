94a839db708cdb23217644049b51660f649de3e0
Landon Shumway <landon@inspiringapps.com>
2025-05-22 11:45:44 -0500

    Split out status calculations between license and provider

--- backend/compact-connect/lambdas/python/common/cc_common/data_model/schema/provider/record.py
+++ backend/compact-connect/lambdas/python/common/cc_common/data_model/schema/provider/record.py
@@ -1,13 +1,19 @@
 # ruff: noqa: N801, N815, ARG002  invalid-name unused-argument
+from datetime import date
 from urllib.parse import quote
 
-from marshmallow import post_dump, post_load, pre_dump
+from marshmallow import post_dump, post_load, pre_dump, pre_load
 from marshmallow.fields import UUID, Date, DateTime, Email, List, Nested, String
 from marshmallow.validate import Length, Regexp
 
 from cc_common.config import config
-from cc_common.data_model.schema.base_record import BaseRecordSchema, CalculatedStatusRecordSchema, ForgivingSchema
-from cc_common.data_model.schema.common import ChangeHashMixin
+from cc_common.data_model.schema.base_record import BaseRecordSchema, ForgivingSchema
+from cc_common.data_model.schema.common import (
+    ActiveInactiveStatus,
+    ChangeHashMixin,
+    CompactEligibilityStatus,
+    LicenseEncumberedStatusEnum,
+)
 from cc_common.data_model.schema.fields import (
     UNKNOWN_JURISDICTION,
     ActiveInactive,
@@ -24,7 +30,7 @@ from cc_common.data_model.schema.fields import (
 
 
 @BaseRecordSchema.register_schema('provider')
-class ProviderRecordSchema(CalculatedStatusRecordSchema):
+class ProviderRecordSchema(BaseRecordSchema):
     """
     Schema for provider records in the provider data table
 
@@ -42,6 +48,10 @@ class ProviderRecordSchema(CalculatedStatusRecordSchema):
 
     jurisdictionUploadedLicenseStatus = ActiveInactive(required=True, allow_none=False)
     jurisdictionUploadedCompactEligibility = CompactEligibility(required=True, allow_none=False)
+
+    # Calculated fields
+    licenseStatus = ActiveInactive(required=True, allow_none=False)
+    compactEligibility = CompactEligibility(required=True, allow_none=False)
 
     # optional field for setting encumbrance status
     encumberedStatus = LicenseEncumberedStatusField(required=False, allow_none=False)
@@ -77,6 +87,54 @@ class ProviderRecordSchema(CalculatedStatusRecordSchema):
     currentHomeJurisdiction = CurrentHomeJurisdictionField(
         required=False, allow_none=False, load_default=UNKNOWN_JURISDICTION
     )
+
+    @pre_load
+    def _calculate_statuses(self, in_data, **_kwargs):
+        """Determine the statuses of the record based on the expiration date"""
+        in_data = self._calculate_license_status(in_data)
+        return self._calculate_compact_eligibility(in_data)
+
+    def _calculate_license_status(self, in_data, **_kwargs):
+        """Determine the status of the license based on the expiration date"""
+        in_data['licenseStatus'] = (
+            ActiveInactiveStatus.ACTIVE
+            if (
+                in_data['jurisdictionUploadedLicenseStatus'] == ActiveInactiveStatus.ACTIVE
+                and date.fromisoformat(in_data['dateOfExpiration']) >= config.expiration_resolution_date
+                and in_data.get('encumberedStatus', LicenseEncumberedStatusEnum.UNENCUMBERED)
+                == LicenseEncumberedStatusEnum.UNENCUMBERED
+            )
+            else ActiveInactiveStatus.INACTIVE
+        )
+        return in_data
+
+    def _calculate_compact_eligibility(self, in_data, **_kwargs):
+        """
+        Providers are only eligible for the compact if their home jurisdiction says they are, none of their licenses
+        are encumbered, and their license is active.
+        """
+        in_data['compactEligibility'] = (
+            CompactEligibilityStatus.ELIGIBLE
+            if (
+                in_data['jurisdictionUploadedCompactEligibility'] == CompactEligibilityStatus.ELIGIBLE
+                and in_data['licenseStatus'] == ActiveInactiveStatus.ACTIVE
+                and in_data.get('encumberedStatus', LicenseEncumberedStatusEnum.UNENCUMBERED)
+                == LicenseEncumberedStatusEnum.UNENCUMBERED
+                # in the case of providers, we set this value if the provider moved to a jurisdiction where they do
+                # not have a valid license within the compact member states.
+                and in_data.get('homeJurisdictionChangeDeactivationStatus', None) is None
+            )
+            else CompactEligibilityStatus.INELIGIBLE
+        )
+        return in_data
+
+    @pre_dump
+    def remove_calculated_fields(self, in_data, **_kwargs):
+        """Remove the calculated status fields before dumping to the database"""
+        in_data.pop('status', None)
+        in_data.pop('licenseStatus', None)
+        in_data.pop('compactEligibility', None)
+        return in_data
 
     @pre_dump
     def generate_pk_sk(self, in_data, **kwargs):  # noqa: ARG001 unused-argument
