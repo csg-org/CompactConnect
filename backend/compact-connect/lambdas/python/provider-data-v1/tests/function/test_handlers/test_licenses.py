b0fcde2b3198c3cbdd793af37e0d4dc5fc2501d8
Landon Shumway <landon@inspiringapps.com>
2025-07-16 09:26:45 -0500

    Apply request schema to license upload endpoints

--- backend/compact-connect/lambdas/python/provider-data-v1/tests/function/test_handlers/test_licenses.py
+++ backend/compact-connect/lambdas/python/provider-data-v1/tests/function/test_handlers/test_licenses.py
@@ -54,3 +54,83 @@ class TestLicenses(TstFunction):
         resp = post_licenses(event, self.mock_context)
 
         self.assertEqual(400, resp['statusCode'])
+        self.assertEqual(
+            {'message': {'0': {'licenseType': ['Must be one of: audiologist, speech-language pathologist.']}}},
+            json.loads(resp['body']),
+        )
+
+    def test_post_licenses_unknown_field_returns_error(self):
+        from handlers.licenses import post_licenses
+
+        with open('../common/tests/resources/api-event.json') as f:
+            event = json.load(f)
+
+        # The user has write permission for aslp/oh
+        event['requestContext']['authorizer']['claims']['scope'] = 'openid email aslp/readGeneral oh/aslp.write'
+        event['pathParameters'] = {'compact': 'aslp', 'jurisdiction': 'oh'}
+        with open('../common/tests/resources/api/license-post.json') as f:
+            license_data = json.load(f)
+            license_data['someOtherField'] = 'foobar'
+        event['body'] = json.dumps([license_data])
+
+        resp = post_licenses(event, self.mock_context)
+
+        self.assertEqual(400, resp['statusCode'])
+        self.assertEqual({'message': {'0': {'someOtherField': ['Unknown field.']}}}, json.loads(resp['body']))
+
+    def test_post_licenses_strips_whitespace_from_string_fields(self):
+        """Test that whitespace is stripped from all string fields in license data."""
+        from handlers.licenses import post_licenses
+
+        with open('../common/tests/resources/api-event.json') as f:
+            event = json.load(f)
+
+        # The user has write permission for aslp/oh
+        event['requestContext']['authorizer']['claims']['scope'] = 'openid email aslp/readGeneral oh/aslp.write'
+        event['pathParameters'] = {'compact': 'aslp', 'jurisdiction': 'oh'}
+
+        # Load base license data and add whitespace to string fields
+        with open('../common/tests/resources/api/license-post.json') as f:
+            license_data = json.load(f)
+            request_body = license_data.copy()
+
+        # Add whitespace around various string fields
+        request_body['givenName'] = '  ' + license_data['givenName'] + '  '
+        request_body['familyName'] = '  ' + license_data['familyName'] + '  '
+        request_body['licenseType'] = '  ' + license_data['licenseType'] + '  '
+        request_body['homeAddressStreet1'] = '  ' + license_data['homeAddressStreet1'] + '  '
+        request_body['homeAddressCity'] = '  ' + license_data['homeAddressCity'] + '  '
+        request_body['homeAddressState'] = '  ' + license_data['homeAddressState'] + '  '
+        request_body['homeAddressPostalCode'] = '  ' + license_data['homeAddressPostalCode'] + '  '
+
+        # Add optional fields with whitespace
+        request_body['middleName'] = '  ' + license_data['middleName'] + '  '
+        request_body['suffix'] = '  ' + license_data.get('suffix', 'Jr.') + '  '
+        request_body['licenseNumber'] = '  ' + license_data['licenseNumber'] + '  '
+        request_body['emailAddress'] = '  ' + license_data['emailAddress'] + '  '
+
+        event['body'] = json.dumps([request_body])
+
+        resp = post_licenses(event, self.mock_context)
+        self.assertEqual(200, resp['statusCode'])
+
+        # Verify the message was sent to the preprocessing queue with trimmed data
+        queue_messages = self._license_preprocessing_queue.receive_messages(MaxNumberOfMessages=10)
+        self.assertEqual(1, len(queue_messages))
+
+        message_data = json.loads(queue_messages[0].body)
+
+        # Verify that whitespace was stripped from all string fields
+        self.assertEqual(license_data['givenName'], message_data['givenName'])  # Should be trimmed
+        self.assertEqual(license_data['familyName'], message_data['familyName'])  # Should be trimmed
+        self.assertEqual(license_data['licenseType'], message_data['licenseType'])  # Should be trimmed
+        self.assertEqual(license_data['homeAddressStreet1'], message_data['homeAddressStreet1'])  # Should be trimmed
+        self.assertEqual(license_data['homeAddressCity'], message_data['homeAddressCity'])  # Should be trimmed
+        self.assertEqual(license_data['homeAddressState'], message_data['homeAddressState'])  # Should be trimmed
+        self.assertEqual(
+            license_data['homeAddressPostalCode'], message_data['homeAddressPostalCode']
+        )  # Should be trimmed
+        self.assertEqual(license_data['middleName'], message_data['middleName'])  # Should be trimmed
+        self.assertEqual(license_data.get('suffix', 'Jr.'), message_data['suffix'])  # Should be trimmed
+        self.assertEqual(license_data['licenseNumber'], message_data['licenseNumber'])  # Should be trimmed
+        self.assertEqual(license_data['emailAddress'], message_data['emailAddress'])  # Should be trimmed
