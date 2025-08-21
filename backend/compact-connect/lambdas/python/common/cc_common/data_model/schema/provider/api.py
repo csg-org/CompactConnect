31e4e1bcc03e9b6b560187e0575c9df4122d7498
Landon Shumway <landon@inspiringapps.com>
2025-08-21 15:22:37 -0500

    sort imports

--- backend/compact-connect/lambdas/python/common/cc_common/data_model/schema/provider/api.py
+++ backend/compact-connect/lambdas/python/common/cc_common/data_model/schema/provider/api.py
@@ -2,7 +2,7 @@
 from datetime import timedelta
 
 from marshmallow import ValidationError, validates_schema
-from marshmallow.fields import Date, DateTime, Email, Integer, List, Nested, Raw, String, UUID
+from marshmallow.fields import UUID, Date, DateTime, Email, Integer, List, Nested, Raw, String
 from marshmallow.validate import Length, OneOf, Regexp
 
 from cc_common.data_model.schema.base_record import ForgivingSchema
