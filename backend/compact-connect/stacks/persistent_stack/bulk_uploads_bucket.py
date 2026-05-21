37586d6bb09888f41dc964d5984067c3da4f0b2c
Landon Shumway <landon@inspiringapps.com>
2026-05-21 11:25:56 -0500

    remove unused import

--- backend/compact-connect/stacks/persistent_stack/bulk_uploads_bucket.py
+++ backend/compact-connect/stacks/persistent_stack/bulk_uploads_bucket.py
@@ -16,7 +16,6 @@ from cdk_nag import NagSuppressions
 from common_constructs.access_logs_bucket import AccessLogsBucket
 from common_constructs.bucket import Bucket
 from common_constructs.python_function import PythonFunction
-from common_constructs.stack import Stack
 from constructs import Construct
 
 import stacks.persistent_stack as ps
